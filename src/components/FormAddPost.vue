8aa9017c7660c51a594f742b4760bc5b31f67414
Emmily kathylen <emmilysouzakathylen@gmail.com>
2023-05-31 19:22:15 -0300

    documentando e ajustando

--- src/components/FormAddPost.vue
+++ src/components/FormAddPost.vue
@@ -34,6 +34,7 @@ export default {
     },
     methods:{
         addPost () {
+            console.log("alo");
         }        
     }
 }
@@ -41,13 +42,15 @@ export default {
 
 <style scoped>
 form {
-  background-color: #091428;
-  display: flex;
-  flex-direction: column;
-  width: 30vw;
-  height: 450px;
-  margin-bottom: 40px;
-  padding: 25px;
+  background-color: #091428; /* Cor de fundo do fortmulário */
+  display: flex; /* Aplica um layout flexível como vertical */
+  flex-direction: column; /* Define a direção do layout flexível como vertical */
+  width: 35vw; /* Define a largura do formulário */
+  height: 550px; /* Define a altura do formulário */
+  margin-bottom: 65px; /* Adiciona uma margem ao formulário, criando um espaçamento */
+  padding: 25px; /* Define um preenchimento interno ao redor do conteúdo do formulário */
+  font-size: 1.3rem; /* Define o tamando da fonte de texto */
+
 }
 .postar {
   font-size: 5rem;
@@ -59,7 +62,16 @@ form {
   align-items: center;
   width: 80vw;
   height: auto;
+  margin-left: 10vh;
   border-bottom: 1px solid #A09B8C;
 }
+
+ button {
+  font-size: 2rem;
+  width: 15vw;
+  border-radius: 55px;
+
+ }
+
 
 </style>
