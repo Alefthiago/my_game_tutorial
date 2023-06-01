ffd97a7c88a654c1cea98e89a16774c5945c56af
Emmily kathylen <emmilysouzakathylen@gmail.com>
2023-06-01 17:16:20 -0300

    form att

--- src/components/FormAddPost.vue
+++ src/components/FormAddPost.vue
@@ -5,15 +5,15 @@
         <form @submit.prevent="addPost" class="border p-4  border rounded">
           <div class="mb-3">
             <label class="form-label fontBold">Titulo:</label>
-            <input v-model="postTitle" type="text" class="form-control fontItalic" id="exampleFormControlInput1">
+            <input v-model="postTitle" type="text" class="form-control fontItalic" id="exampleFormControlInput1" placeholder="digite aqui...">
           </div>
           <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label fontBold">Conteudo:</label>
-            <textarea v-model="postContent"  class="form-control fontItalic" rows="5"></textarea>
+            <textarea v-model="postContent"  class="form-control fontItalic" rows="5" placeholder="digite aqui..."></textarea>
           </div>
           <div class="mb-3">
             <label class="form-label fontBold">Link do video ou imagem:</label>
-            <input v-model="postLink" type="text" class="form-control fontItalic">
+            <input v-model="postLink" type="text" class="form-control fontItalic" placeholder="digite aqui...">
           </div>
           <div class="mb-3">
             <button type="submit" class="btn btn-secondary fontItalic">Postar</button>
@@ -42,14 +42,15 @@ export default {
 
 <style scoped>
 form {
-  background-color: #091428; /* Cor de fundo do fortmulário */
+  background-color: white; /* Cor de fundo do fortmulário */
   display: flex; /* Aplica um layout flexível como vertical */
   flex-direction: column; /* Define a direção do layout flexível como vertical */
-  width: 35vw; /* Define a largura do formulário */
+  width: 39vw; /* Define a largura do formulário */
   height: 550px; /* Define a altura do formulário */
   margin-bottom: 65px; /* Adiciona uma margem ao formulário, criando um espaçamento */
   padding: 25px; /* Define um preenchimento interno ao redor do conteúdo do formulário */
   font-size: 1.3rem; /* Define o tamando da fonte de texto */
+  
 
 }
 .postar {
@@ -64,14 +65,29 @@ form {
   height: auto;
   margin-left: 10vh;
   border-bottom: 1px solid #A09B8C;
+  
+}
+
+.fontItalic{
+  color: white;
+  border: 0;
+  border-bottom: 2px solid rgb(200, 200, 200);
+  outline: 0;
+  color: black;
+}   
+
+.fontBold{
+  color: black;
 }
 
  button {
   font-size: 2rem;
-  width: 15vw;
+  width: 12vw;
   border-radius: 55px;
-
+  margin-top: 25px; 
+  background-color: #00BFFF;
  }
 
+ 
 
 </style>
